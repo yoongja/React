3a03dea9b6eff0a27c91e9755c5c50a995f6a595
= <rmatlr0112@naver.com>
2022-03-21 15:34:46 +0900

    use effect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,9 +1,18 @@
-import Button from "./Button";
+import { useState , useEffect } from "react";
+  
 function App() { //app함수의 반환값이 많아지면 소괄호로 감싼다
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev)=>prev + 1);
+  console.log("i run all the time");
+  
+  useEffect(() => {
+    console.log("CALL THE API...")
+  },[]); //component가 처음 render할 때 실행되고 다시는 실행되지 않을 function을 넣어준다.
+  
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text ={"continue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>clickme</button>
     </div>
   );
 }
