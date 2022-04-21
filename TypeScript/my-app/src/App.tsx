724e4d817b711410d83e806f96069cf0d994701f
= <rmatlr0112@naver.com>
2022-04-22 00:43:17 +0900

    Ts를 event에 적용

--- TypeScript/my-app/src/App.tsx
+++ TypeScript/my-app/src/App.tsx
@@ -1,10 +1,31 @@
-import Circle from"./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {//interface React.FormEvent<T = Element> -> 어떤종류의 element가 onChange 이벤트를 발생시킬지 특정할 수 있다
+      //TS는 onChange 함수가 inputElement에 의해서 실행될 것을 알게된다.
+      //event.currentTarget.value
+      const {
+        currentTarget: { value },
+      } = event; //event를 여는 방법
+      setValue(value);
+    };
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      console.log("hello", value);
+    };
+  }; 
   return (
     <div>
-      <Circle borderColor = "black" bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
 
   );
