474dd388e937e2b28dd16acbfddef751e09fc040
= <rmatlr0112@naver.com>
2022-04-21 17:55:21 +0900

    4월21일

--- TypeScript/my-app/src/Circle.tsx
+++ TypeScript/my-app/src/Circle.tsx
@@ -2,23 +2,30 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor : string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>` //Container styled component에게 TypeScript한테 circleProps를 받을 것이라는 걸 명시해줌
   width: 200px;
   height: 200px;
-  background-color: ${(props) => props.bgColor};
+  background-color: ${(props) => props.bgColor}; //props를 통해서 bgColor를 받기
   border-radius: 100px;
 `;
 
 interface CircleProps {
   bgColor: string;
-} //TypeScript에게 bgColor는 string이 되어야 한다고 설명해준다.
+  borderColor?: string;
+  text?:string;
+} //TypeScript에게 bgColor는 string이 되어야 한다고 설명해준다.(optional props)
 
 
-
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />; // Circle은 bgColor prop을 받아서 Container에 prop으로 동일하게 보내준다.
-}
+//component에게 Typescript사용하는 것 알려주기
+function Circle({ bgColor ,borderColor, text = "default text"}: CircleProps) {
+  return (
+  <Container bgColor={bgColor} borderColor={borderColor ?? "green"}>
+    {text}
+  </Container>
+  ) ; // Circle은 bgColor prop을 받아서 Container에 prop으로 동일하게 보내준다.
+}//undefined이면 사용할 초기값을 설정해주기
 
 export default Circle;
